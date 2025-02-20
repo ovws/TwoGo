ff60e119bc0c1aab20ed1f6f3a36021adea172a4
v_wensqi <v_wensqi@tencent.com>
2025-02-20 14:32:14 +0800

    feat: 重构文件读取方式并添加Go语言使用说明

--- fileChange/main.go
+++ fileChange/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -29,16 +32,58 @@ func readall() {
 		return
 	}
 	defer fileObj.Close()
-	var tmp = make([]byte, 128)
-	n, err := fileObj.Read(tmp)
+	for {
+
+		var tmp = make([]byte, 128)
+		n, err := fileObj.Read(tmp)
+		if err == io.EOF {
+			fmt.Println(string(tmp[:n]))
+			return
+		}
+		if err != nil {
+			fmt.Println("read file err:", err)
+			return
+		}
+		// fmt.Println(n)
+		fmt.Println(string(tmp[:n]))
+	}
+}
+
+func readByBufio() {
+	fileObj, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("open file err:", err)
+		return
+	}
+	defer fileObj.Close()
+	reader := bufio.NewReader(fileObj)
+	for {
+		read, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Println("read end")
+			return
+		}
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			return
+		}
+		fmt.Print(read)
+	}
+}
+
+func readByIoUtil() {
+	ioutil.ReadFile("test.txt")
+	content, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println("read file err:", err)
 		return
 	}
-	fmt.Println(n)
-	fmt.Println(string(tmp[:n]))
+	fmt.Println(string(content))
 }
 
 func main() {
-
+	// readFromFile()
+	// readall()
+	// readByBufio()
+	readByIoUtil()
 }
